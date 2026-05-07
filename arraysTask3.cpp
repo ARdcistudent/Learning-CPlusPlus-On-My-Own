59523eaaf928d2a49ac1b903e5f850ed7338b32e
ARdcistudent <andre.ruehmling@googlemail.com>
2026-05-07 22:16:32 +0200

    Arrays Task3 done

--- arraysTask3.cpp
+++ arraysTask3.cpp
@@ -25,3 +25,34 @@
 // 99
 
 // → false
+
+#include <iostream>
+
+bool enthaeltZahl(int zahl, int array[], int arrayGroesse) {
+    bool statement = false; 
+    for (int i = 0; i < arrayGroesse; i++) 
+    {
+        if (array[i] == zahl) { 
+            statement = true; 
+            break;
+        }  
+    }
+    
+    if (statement) {
+        std::cout << "True!";
+    } else {
+        std::cout << "False!";
+    }
+    
+    return statement;
+}
+
+int main() {
+    int zahlen[] = {4, 8, 15, 16, 23, 42};
+    enthaeltZahl(15,zahlen,6);
+    std::cout << std::endl;
+    enthaeltZahl(99,zahlen,6);
+    std::cout << std::endl;
+
+    return 0;
+}
