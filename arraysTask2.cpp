b9e6d7e51d589daf3af73f5b8f19dd86320e0372
ARdcistudent <andre.ruehmling@googlemail.com>
2026-05-07 20:30:46 +0200

    Arrays Task2 done

--- arraysTask2.cpp
+++ arraysTask2.cpp
@@ -15,3 +15,22 @@
 // 75
 
 // (Hinweis: Hier musst du überlegen, wie man ein Array an Funktionen übergibt. Kleiner Denkstein 🪨😄)
+
+#include <iostream>
+template<size_t N>
+
+int additionArray(int (&array)[N]){
+    int summe = 0;
+    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    {
+        summe += array[i];
+    }
+    return summe;
+}
+
+int main() {
+    int zahlen[5] = {5, 10, 15, 20, 25};
+    std::cout << additionArray(zahlen) << std::endl;
+
+    return 0;
+}
