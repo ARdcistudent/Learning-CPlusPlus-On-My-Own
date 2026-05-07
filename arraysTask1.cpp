302556c29a335b8ec72e355cd4ee7f7053da80db
ARdcistudent <andre.ruehmling@googlemail.com>
2026-05-07 18:38:53 +0200

    Arrays Task 1 done

--- arraysTask1.cpp
+++ arraysTask1.cpp
@@ -5,3 +5,16 @@
 // int zahlen[5] = {1, 2, 3, 4, 5};
 
 // Gib mit Schleife alle Zahlen aus.
+
+#include <iostream>
+
+int zahlen[5] = {1, 2, 3, 4, 5};
+
+int main() {
+    for (int i = 0; i < sizeof(zahlen) / sizeof(zahlen[0]); i++)
+    {
+        std::cout << zahlen[i] << "\n";
+    }
+    
+    return 0;
+}
