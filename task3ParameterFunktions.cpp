89c3c49514afcb0b7c357fa7a5ccb082c9b2ed08
ARdcistudent <andre.ruehmling@googlemail.com>
2026-05-07 17:40:36 +0200

    Functions with Parameters, Task3 done!

--- task3ParameterFunktions.cpp
+++ task3ParameterFunktions.cpp
@@ -11,3 +11,23 @@
 // *****
 
 // 👉 also mehrere Reihen
+
+#include <iostream>
+
+void customBox(int breite) {
+    for (int i = 0; i < breite/2; i++)
+    {
+        std::cout << "\n";
+        for (size_t i = 0; i < breite; i++)
+        {
+            std::cout << "*";
+        }
+    }
+    std::cout << "\n";
+}
+
+int main() {
+    customBox(6);
+
+    return 0;
+}
