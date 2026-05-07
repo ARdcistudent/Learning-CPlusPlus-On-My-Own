f7bc702d7f81d2bbc0f6e66bc7e0668f2059bd02
ARdcistudent <andre.ruehmling@googlemail.com>
2026-05-07 18:17:59 +0200

    Rückgabewerte Tasks done

--- rueckgabewerteTask3.cpp
+++ rueckgabewerteTask3.cpp
@@ -30,5 +30,30 @@
 #include <iostream>
 
 bool istGerade(int zahl) {
-    
+    bool statement = zahl % 2 == 0;
+
+    return statement;
+}
+
+// Oder auch, damit es übersichtlicher in main() ist:
+
+void ifIstGerade(int zahl) {
+    if(istGerade(zahl)){
+        std::cout << "Gerade" << std::endl;
+    } else {
+        std::cout << "Ungerade" << std::endl;
+    }
+}
+
+int main() {
+    if(istGerade(8)){
+        std::cout << "Gerade" << std::endl;
+    } else {
+        std::cout << "Ungerade" << std::endl;
+    }
+
+    //hier die alternative:
+    ifIstGerade(7);
+
+    return 0;
 }
